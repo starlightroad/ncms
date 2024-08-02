30289605c76f2fbe80e16c18a3ee591cde3f0066
starlightroad <ortizge92@gmail.com>
2024-08-02 19:33:10 -0400

    Create app logo

---
+++ app/ui/logo.tsx
@@ -0,0 +1,9 @@
+import { WaypointsIcon } from 'lucide-react';
+
+export default function Logo() {
+  return (
+    <span>
+      <WaypointsIcon className="h-5 w-5 text-gray-900" />
+    </span>
+  );
+}
