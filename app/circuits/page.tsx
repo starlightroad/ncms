12af40f3d3338063d392b27687cc82d09e7c0bdd
starlightroad <ortizge92@gmail.com>
2024-08-03 21:23:03 -0400

    Add circuits page metadata

--- app/circuits/page.tsx
+++ app/circuits/page.tsx
@@ -1,7 +1,12 @@
+import type { Metadata } from 'next';
 import { getCircuits } from '@/app/data/circuit';
 import PageHeader from '@/app/ui/page-header';
 import PageHeading from '@/app/ui/page-heading';
 import CircuitsTable from '@/app/ui/circuits/table';
+
+export const metadata: Metadata = {
+  title: 'Circuits',
+};
 
 export default function Circuits() {
   const circuits = getCircuits();
