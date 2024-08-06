412a6bcba8ba24301b7bb9a545a568b444816d3e
starlightroad <ortizge92@gmail.com>
2024-08-05 22:18:46 -0400

    Update the sidebar theme

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -2,32 +2,45 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import {
+  ArrowDownUpIcon,
+  Building2Icon,
+  LayoutDashboardIcon,
+  MapIcon,
+  MapPinIcon,
+} from 'lucide-react';
 
 const navItems = [
   {
     id: '26c211a8-ab0e-4c54-accd-1de3da2ab2d8',
     label: 'Dashboard',
     href: '/',
+    icon: LayoutDashboardIcon,
   },
   {
     id: '65288da9-356f-4f31-82c7-6056f8e9c954',
     label: 'Circuits',
     href: '/circuits',
+    icon: ArrowDownUpIcon,
   },
   {
     id: '4458cf3a-dac1-4217-a88c-7aec4a3dc04b',
     label: 'Locations',
     href: '/locations',
+    icon: MapPinIcon,
   },
   {
     id: '3f1e2b91-7f0a-43e7-a66f-9102da5d6e20',
     label: 'Vendors',
     href: '/vendors',
+    icon: Building2Icon,
   },
   {
     id: '35a4e565-6f8c-4cf8-9ba6-008d44d29c1f',
     label: 'Map',
     href: '/map',
+    icon: MapIcon,
   },
 ];
 
@@ -41,17 +54,19 @@ export default function Sidebar() {
           {navItems.map((navItem) => {
             const href = `/${pathname.split('/').slice(1)[0]}` || '/';
             const activeLink = href === navItem.href;
+            const Icon = navItem.icon;
 
             return (
               <li key={navItem.id}>
                 <Link
                   href={navItem.href}
-                  className={`relative flex w-full rounded-md py-3 hover:text-pink-600 ${activeLink ? 'bg-pink-600 bg-opacity-[6%] font-medium' : 'text-gray-600'}`}
+                  className={clsx(
+                    'flex w-full items-center rounded-full px-4 py-3 text-gray-600 transition-colors hover:text-blue-600',
+                    { 'bg-blue-50 font-medium': activeLink, 'hover:bg-gray-50': !activeLink },
+                  )}
                 >
-                  <span
-                    className={`absolute top-2 h-[calc(100%-16px)] rounded-b-sm rounded-t-sm border-x-2 ${activeLink ? 'border-pink-600' : 'border-transparent'}`}
-                  ></span>
-                  <p className={`pl-4 text-sm ${activeLink ? 'text-pink-600' : ''}`}>
+                  <Icon className={clsx('h-4 w-4', { 'text-blue-600': activeLink })} />
+                  <p className={clsx('pl-2 text-sm', { 'text-blue-600': activeLink })}>
                     {navItem.label}
                   </p>
                 </Link>
