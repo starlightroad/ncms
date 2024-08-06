548d842fa3b8c04ee94fd2092d7f88717f1e91a6
starlightroad <ortizge92@gmail.com>
2024-08-05 21:39:38 -0400

    Build the new circuit form

--- app/circuits/page.tsx
+++ app/circuits/page.tsx
@@ -2,6 +2,16 @@ import type { Metadata } from 'next';
 import PageHeader from '@/app/ui/page-header';
 import PageHeading from '@/app/ui/page-heading';
 import CircuitsTable from '@/app/ui/circuits/table';
+import { Button } from '@/app/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/app/ui/dialog';
+import AddCircuitForm from '@/app/ui/circuits/add-form';
 
 export const metadata: Metadata = {
   title: 'Circuits',
@@ -11,7 +21,27 @@ export default function Circuits() {
   return (
     <main>
       <PageHeader>
-        <PageHeading>Circuits</PageHeading>
+        <div className="flex items-center justify-between">
+          <PageHeading>Circuits</PageHeading>
+          <div className="space-x-2">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size="sm">New Circuit</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>New Circuit</DialogTitle>
+                </DialogHeader>
+                <AddCircuitForm />
+                <DialogFooter>
+                  <Button type="submit" form="add-form" size="sm">
+                    Create
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
       </PageHeader>
       <CircuitsTable />
     </main>
