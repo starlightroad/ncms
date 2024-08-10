c45b7833eb2da028d7be5d53a6cbd473cf0901cd
starlightroad <ortizge92@gmail.com>
2024-08-10 19:23:09 -0400

    Add a utility to mock slow data fetching

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -27,3 +27,17 @@ export const formatAddress = (address: Address, options?: { format: 'short' | 'f
 
   return `${street}, ${city}, ${state} ${zip}`;
 };
+
+export const mockSlowDataFetching = async <T>(
+  fn: () => Promise<any>,
+  seconds?: number,
+): Promise<T> => {
+  return await new Promise((res) => {
+    setTimeout(
+      async () => {
+        return res(await fn());
+      },
+      1000 * (seconds ?? 2),
+    );
+  });
+};
