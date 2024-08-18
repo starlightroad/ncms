79641b810b5ea652d827e480e3c7fc94b7afa1df
starlightroad <ortizge92@gmail.com>
2024-08-17 22:38:21 -0400

    Update the circuit details page

--- app/circuits/[id]/page.tsx
+++ app/circuits/[id]/page.tsx
@@ -1,23 +1,14 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 import { ArrowDownUpIcon, Edit2Icon, MapIcon, Trash2Icon } from 'lucide-react';
 import PageHeader from '@/app/ui/page-header';
 import PageHeading from '@/app/ui/page-heading';
 import { Button } from '@/app/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/app/ui/card';
-import EditCircuitForm from '@/app/ui/circuits/edit-form';
-import DeleteCircuitForm from '@/app/ui/circuits/delete-form';
 import { formatAddress } from '@/app/lib/utils';
 import { getCircuit } from '@/app/data/circuit';
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/app/ui/dialog';
+import DeleteCircuitModal from '@/app/ui/circuits/delete-modal';
 
 type Props = {
   params: { id: string };
@@ -25,31 +16,32 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { id } = params;
-  const circuit = getCircuit(Number(id));
+  const circuit = await getCircuit(id);
 
   return {
-    title: String(circuit?.id) ?? 'Not Found',
+    title: circuit?.cid ?? 'Not Found',
   };
 }
 
-export default function Circuit({ params }: { params: { id: string } }) {
-  const data = getCircuit(Number(params.id));
+const DeleteButton = (
+  <Button type="button" size="sm" variant="outline" className="gap-1">
+    <Trash2Icon className="h-4 w-4" />
+    Delete
+  </Button>
+);
+
+export default async function Circuit({ params }: { params: { id: string } }) {
+  const data = await getCircuit(params.id);
 
   if (!data) {
-    return (
-      <main>
-        <PageHeader>
-          <PageHeading>Circuit With ID {params.id} Was Not Found</PageHeading>
-        </PageHeader>
-      </main>
-    );
+    return notFound();
   }
 
-  const locationA = formatAddress(data.location.a);
-  const locationZ = formatAddress(data.location.z);
+  const locationA = formatAddress(data.location1);
+  const locationZ = formatAddress(data.location2);
 
   const items = [
-    { label: 'vendor', description: data.name },
+    { label: 'vendor', description: data.vendor.name },
     { label: 'type', description: data.type },
     { label: 'capacity', description: data.capacity },
     { label: 'a location', description: locationA },
@@ -60,51 +52,15 @@ export default function Circuit({ params }: { params: { id: string } }) {
     <main>
       <PageHeader>
         <div className="flex items-center justify-between">
-          <PageHeading>{params.id}</PageHeading>
+          <PageHeading>{data.cid}</PageHeading>
           <div className="space-x-2">
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button type="button" size="sm" variant="outline" className="gap-1">
-                  <Edit2Icon className="h-4 w-4" />
-                  Edit
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Edit Circuit</DialogTitle>
-                </DialogHeader>
-                <EditCircuitForm circuit={data} />
-                <DialogFooter>
-                  <Button type="submit" form="edit-form">
-                    Update
-                  </Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button type="button" size="sm" variant="outline" className="gap-1">
-                  <Trash2Icon className="h-4 w-4 text-gray-600" />
-                  Delete
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Delete Circuit</DialogTitle>
-                  <DialogDescription>
-                    Are you sure you want to delete this circuit?
-                  </DialogDescription>
-                </DialogHeader>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button type="button" variant="outline" size="sm">
-                      Cancel
-                    </Button>
-                  </DialogClose>
-                  <DeleteCircuitForm circuitId={data.id} />
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
+            <Button type="button" size="sm" variant="outline" className="gap-1" asChild>
+              <Link href={`/circuits/${data.id}/edit`}>
+                <Edit2Icon className="h-4 w-4" />
+                Edit
+              </Link>
+            </Button>
+            <DeleteCircuitModal circuitId={data.id} trigger={DeleteButton} />
           </div>
         </div>
       </PageHeader>
