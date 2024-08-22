a3f51af4676b243898d2db534261d8a5c9684316
starlightroad <ortizge92@gmail.com>
2024-08-21 21:37:46 -0400

    Commit the missing location data functions

--- app/data/location.ts
+++ app/data/location.ts
@@ -1,4 +1,6 @@
 import prisma from '@/prisma/client';
+import { ITEMS_PER_PAGE } from '@/app/lib/constants';
+import { getDemoCompanyId } from '@/app/data/demo';
 
 export const getLocations = async () => {
   try {
@@ -15,5 +17,35 @@ export const getLocation = async (locationId: string) => {
     return location;
   } catch (error) {
     throw new Error('Failed to fetch location.');
+  }
+};
+
+export const getLocationPages = async () => {
+  try {
+    const count = await prisma.location.count();
+    const pages = Math.ceil(count / ITEMS_PER_PAGE);
+
+    return pages;
+  } catch (error) {
+    throw new Error('Failed to Fetch Number of Locations.');
+  }
+};
+
+export const getFilteredLocations = async (currentPage: number) => {
+  const demoCompanyId = await getDemoCompanyId();
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE;
+  const take = currentPage * ITEMS_PER_PAGE;
+
+  try {
+    const locations = await prisma.location.findMany({
+      where: {
+        companyId: demoCompanyId,
+      },
+      skip,
+      take,
+    });
+    return locations;
+  } catch (error) {
+    throw new Error('Failed to Get Filtered Locations.');
   }
 };
