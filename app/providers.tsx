e383894b73812822ff628b9967f7b86a2c58d86b
starlightroad <ortizge92@gmail.com>
2024-08-22 22:34:17 -0400

    Build the theme provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,12 @@
 'use client';
 
 import MapProvider from '@/app/providers/map-provider';
+import { ThemeProvider } from '@/app/providers/theme-provider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <MapProvider>{children}</MapProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <MapProvider>{children}</MapProvider>;
+    </ThemeProvider>
+  );
 }
