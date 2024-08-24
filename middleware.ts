061e22dc8f565f14b40c54ea64f95eb1ed3c55f2
starlightroad <ortizge92@gmail.com>
2024-08-24 19:39:07 -0400

    Refactor the auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,28 @@
 import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
+import { NextResponse } from 'next/server';
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth: middleware } = NextAuth(authConfig);
+
+export default middleware(async (request) => {
+  const session = await middleware();
+  const reqUrl = request.nextUrl;
+
+  if (!session?.user) {
+    if (reqUrl.pathname === '/signin' || reqUrl.pathname === '/signup' || reqUrl.pathname === '/') {
+      return;
+    } else {
+      return NextResponse.redirect(new URL('/signin', request.url));
+    }
+  }
+
+  if (reqUrl.pathname === '/signin' || reqUrl.pathname === '/signup') {
+    if (session?.user) {
+      return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+  }
+});
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+};
