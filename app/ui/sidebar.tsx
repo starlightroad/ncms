a840dbaa54b41b68060e2b0448fd5b9da3a4f39e
starlightroad <ortizge92@gmail.com>
2024-08-24 20:19:09 -0400

    Update nav item links and fix active link

--- app/ui/sidebar.tsx
+++ app/ui/sidebar.tsx
@@ -9,48 +9,45 @@ const navItems = [
   {
     id: '26c211a8-ab0e-4c54-accd-1de3da2ab2d8',
     label: 'Dashboard',
-    href: '/',
+    href: '/dashboard',
     icon: LayoutDashboardIcon,
   },
   {
     id: '65288da9-356f-4f31-82c7-6056f8e9c954',
     label: 'Circuits',
-    href: '/circuits',
+    href: '/dashboard/circuits',
     icon: ArrowDownUpIcon,
   },
   {
     id: '4458cf3a-dac1-4217-a88c-7aec4a3dc04b',
     label: 'Locations',
-    href: '/locations',
+    href: '/dashboard/locations',
     icon: MapPinIcon,
   },
   {
     id: '3f1e2b91-7f0a-43e7-a66f-9102da5d6e20',
     label: 'Vendors',
-    href: '/vendors',
+    href: '/dashboard/vendors',
     icon: Building2Icon,
   },
 ];
 
 export default function Sidebar() {
   const pathname = usePathname();
-  const companyName = pathname.split('/')[1];
 
   return (
     <div className="fixed top-14 z-10 hidden h-full w-full max-w-56 border-r bg-background md:block">
       <nav className="py-6">
         <ul className="space-y-1 px-3">
           {navItems.map((navItem) => {
-            const destination = pathname.split('/').slice(1)[1] || '/';
-            const modifiedDestination = destination === '/' ? '/' : `/${destination}`;
-            const activeLink = modifiedDestination === navItem.href;
-            const href = `/${companyName}${navItem.href}`;
+            const path = pathname.split('/').slice(1, 3).join('/');
+            const activeLink = `/${path}` === navItem.href;
             const Icon = navItem.icon;
 
             return (
               <li key={navItem.id}>
                 <Link
-                  href={href}
+                  href={navItem.href}
                   className={clsx(
                     'flex w-full items-center rounded-full px-4 py-3 text-foreground/60 transition-colors hover:text-primary',
                     {
