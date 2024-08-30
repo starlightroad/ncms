4f9d9205ddd739b0ae7aca1dfef312fcda53842e
starlightroad <ortizge92@gmail.com>
2024-08-30 00:09:25 -0400

    Refactor the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,24 @@
 import NextAuth from 'next-auth';
 import authConfig from '@/auth.config';
 import { NextResponse } from 'next/server';
+import { PUBLIC_ROUTES } from '@/app/lib/constants';
 
 const { auth: middleware } = NextAuth(authConfig);
 
 export default middleware(async (request) => {
-  const session = await middleware();
-  const reqUrl = request.nextUrl;
+  const requestUrl = request.nextUrl;
+  const isAuthenticated = !!request.auth;
+  const isPublicRoute = PUBLIC_ROUTES.includes(requestUrl.pathname);
 
-  if (!session?.user) {
-    if (reqUrl.pathname === '/signin' || reqUrl.pathname === '/signup' || reqUrl.pathname === '/') {
-      return;
-    } else {
-      return NextResponse.redirect(new URL('/signin', request.url));
-    }
+  if (!isAuthenticated && !isPublicRoute) {
+    return NextResponse.redirect(new URL('/signin', request.url));
   }
 
-  if (reqUrl.pathname === '/signin' || reqUrl.pathname === '/signup') {
-    if (session?.user) {
-      return NextResponse.redirect(new URL('/dashboard', request.url));
-    }
+  if (isAuthenticated && isPublicRoute && requestUrl.pathname !== '/') {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
+
+  return NextResponse.next();
 });
 
 export const config = {
