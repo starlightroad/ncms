98aea2307188ff9a05709652075665ee52d0c050
starlightroad <ortizge92@gmail.com>
2025-01-22 22:50:19 -0500

    Add demo account credentials to sign in form

--- app/ui/auth/signin-form.tsx
+++ app/ui/auth/signin-form.tsx
@@ -1,18 +1,42 @@
 'use client';
 
+import { type FormEvent, useState } from 'react';
 import { useFormState } from 'react-dom';
 import { Button } from '@/app/ui/button';
 import { Input } from '@/app/ui/input';
 import { Label } from '@/app/ui/label';
 import FormStatusMessage from '@/app/ui/form-status-message';
 import { signInUser } from '@/app/(auth)/actions';
+import { users } from '@/app/data/placeholder-data';
 
 const initialState = {
   message: '',
 };
 
+type UserCredential = Pick<(typeof users)[0], 'email' | 'password'>;
+
+const getDemoUserCredentials = () => {
+  const [demoUser] = users;
+  const { email, password } = demoUser;
+
+  return {
+    email,
+    password,
+  };
+};
+
 export default function SignInForm() {
   const [state, formAction] = useFormState(signInUser, initialState);
+
+  const [demoCredentials, setDemoCredentials] = useState<UserCredential>();
+
+  const fillDemoUserCredentials = (e: FormEvent) => {
+    e.preventDefault();
+
+    const demoUserCredentials = getDemoUserCredentials();
+
+    setDemoCredentials(demoUserCredentials);
+  };
 
   return (
     <form action={formAction}>
@@ -22,13 +46,25 @@ export default function SignInForm() {
           <Label htmlFor="email" className="sr-only">
             Email
           </Label>
-          <Input id="email" type="email" name="email" placeholder="Email" />
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            defaultValue={demoCredentials?.email}
+          />
         </fieldset>
         <fieldset className="mb-3">
           <Label htmlFor="password" className="sr-only">
             Password
           </Label>
-          <Input id="password" type="password" name="password" placeholder="Password" />
+          <Input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            defaultValue={demoCredentials?.password}
+          />
         </fieldset>
         <fieldset>
           <Label htmlFor="confirm-password" className="sr-only">
@@ -44,6 +80,9 @@ export default function SignInForm() {
         </fieldset>
         <Button size="sm" className="w-full">
           Sign In
+        </Button>
+        <Button variant="link" size="sm" onClick={fillDemoUserCredentials}>
+          Use Demo Account
         </Button>
       </div>
     </form>
